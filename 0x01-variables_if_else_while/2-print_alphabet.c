a8bea3449d0b778ab878f8dd3a56a9a93a1da07d
comfycomfort <comfortadekunle24@gmail.com>
2023-10-06 09:16:27 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,8 +8,10 @@
 
 int main(void)
 {
-	for (i  = 'a' ; i <= 'z' ;  i++)
-		 putchar(i);	
-	putchar('\n');
-	return (0);
-} 
+	char i;
+
+for (i  = 'a' ; i <= 'z' ;  i++)
+ putchar(i);	
+putchar('\n');
+return (0);
+}

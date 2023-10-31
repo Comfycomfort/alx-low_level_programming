ba71b7698fe058d79c6d3d7a6ad46793671a8116
comfycomfort <comfortadekunle24@gmail.com>
2023-10-31 07:46:21 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,7 +7,7 @@
  */
 void ch_free_grid(char **grid, size_t height)
 {
-	if (grid != NULL height != 0)
+	if (grid != NULL && height != 0)
 	{
 		for (; height > 0; height--)
 		free(grid[height]);
@@ -32,31 +32,33 @@ char **strtow(char *str)
 	for (c = height = 0; str[c] != '\0'; c++)
 		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 		height++;
+	{
 		aout = malloc(sizeof(char *) * (height = 1));
+	}
 		if (aout == NULL)
 		{
 			free(aout);
 			return (NULL);
 		}
-	for (i = a1 = 0; i < height; i++)
+	for (i = al = 0; i < height; i++)
 	{
-		for (c = a1; str[c] != '\0'; c++)
+		for (c = al; str[c] != '\0'; c++)
 		{
 			if (str[c] == ' ')
-				a1++;
+				al++;
 			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			{
-				aout[1] = malloc((c - a1 + 2) * sizeof(char));
+				aout[1] = malloc((c - al + 2) * sizeof(char));
 				if (aout[i] == NULL)
 				{
 				ch_free_grid(aout, i);
-				retun(NULL);
+				return(NULL);
 				}
 				break;
 			}
 		}
-		for (j = 0; a1 <= c; a1++, j++)
-			aout[i][j] = strr[a1]
+		for (j = 0; al <= c; al++, j++)
+			aout[i][j] = str[al];
 		aout[i][j] = '\0';
 	}
 	aout[i] = NULL;

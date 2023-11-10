197ffbcb89ec180dbf0f376ae0921fe27e262154
comfycomfort <comfortadekunle24@gmail.com>
2023-11-10 02:40:12 -0800

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
+#include "function_pointers.h"
 
 /**
  * print_name - execute a function given as a parameter
- * @name: parameter that function ptr needs
+ * @name: parameter that function pointer needs
  * @f:function to execute
  *
  */
@@ -10,5 +11,7 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
+	{
 		f(name);
+	}
 }
